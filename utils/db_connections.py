bfe68b65bc2606b8e9ee15d0e5ca0dfc53014c4e
Adithya Kashyap <119522695+kashyap-adithya@users.noreply.github.com>
2025-04-08 12:49:00 +0530

    Update db_connections.py
    Signed-off-by: Adithya Kashyap <119522695+kashyap-adithya@users.noreply.github.com>

--- utils/db_connections.py
+++ utils/db_connections.py
@@ -3,13 +3,15 @@ import pandas as pd
 from dotenv import load_dotenv
 import streamlit as st
 
+db = st.secrets["database"]
+
 @st.cache_resource
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT"),
-        sslmode=os.getenv("DB_SSLMODE"),
+        host=db["host"],
+        database=db["name"],
+        user=db["user"],
+        password=db["password"],
+        port=db["port"],
+        sslmode=db["sslmode"],
     )
