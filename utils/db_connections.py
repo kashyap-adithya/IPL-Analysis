2a090b59042ee4feac51c60422e4af169681a901
Adithya Kashyap <119522695+kashyap-adithya@users.noreply.github.com>
2025-04-08 12:43:37 +0530

    Update db_connections.py
    Signed-off-by: Adithya Kashyap <119522695+kashyap-adithya@users.noreply.github.com>

--- utils/db_connections.py
+++ utils/db_connections.py
@@ -1,14 +1,15 @@
 import psycopg2
 import pandas as pd
+from dotenv import load_dotenv
 import streamlit as st
 
 @st.cache_resource
 def get_connection():
     return psycopg2.connect(
-        host="ep-crimson-dream-a127ua7h-pooler.ap-southeast-1.aws.neon.tech",
-        database="neondb",
-        user="neondb_owner",
-        password="npg_xIoS7OUMlQV3",
-        port=5432,
-        sslmode="require",
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+        sslmode=os.getenv("DB_SSLMODE"),
     )
